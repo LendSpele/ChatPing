dee6c95da2ad16b8b04f1120297329ccdc18edb3
LendSpele <lendspelee@gmail.com>
2025-11-11 18:59:12 +0100

    1.21.10

--- src/main/java/org/lend/chatping/Chatping.java
+++ src/main/java/org/lend/chatping/Chatping.java
@@ -2,14 +2,10 @@ package org.lend.chatping;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
-import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
-
 
 public class Chatping implements ModInitializer {
     @Override
@@ -17,32 +13,29 @@ public class Chatping implements ModInitializer {
         ServerMessageEvents.CHAT_MESSAGE.register((message, sender, params) -> {
             String text = message.getContent().getString().toLowerCase();
 
+            MinecraftServer server = sender.getEntityWorld().getServer(); // ГАРАНТИРОВАНО есть!
+
             if (text.contains("@everyone")) {
-                for (ServerPlayerEntity player : sender.getServer().getPlayerManager().getPlayerList()) {
+                for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
                     pingPlayer(player);
                 }
-                return;
-            }
-
-            for (ServerPlayerEntity player : sender.getServer().getPlayerManager().getPlayerList()) {
-                String name = player.getGameProfile().getName().toLowerCase();
-
-                if (text.contains(name)) {
-                    pingPlayer(player);
+            } else {
+                for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                    String name = player.getGameProfile().name().toLowerCase();
+                    if (text.contains(name)) {
+                        pingPlayer(player);
+                    }
                 }
             }
         });
     }
 
     private void pingPlayer(ServerPlayerEntity player) {
-        RegistryEntry<SoundEvent> sound = Registries.SOUND_EVENT.getEntry(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP);
-        player.networkHandler.sendPacket(
-                new PlaySoundS2CPacket(
-                        sound,
-                        SoundCategory.MASTER,
-                        player.getX(), player.getY(), player.getZ(),
-                        1.0F, 1.0F, player.getRandom().nextLong()
-                )
+        player.playSoundToPlayer(
+                SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP,
+                SoundCategory.MASTER,
+                1.0F,
+                1.0F
         );
 
         // System.out.println("[ChatPing] Звук отправлен игроку " + player.getName().getString());
